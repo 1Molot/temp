6b17945776b9bf8c51329dd600bdf2adeffd8667
1Molot <sinkevich1990@gmail.com>
2023-02-21 13:46:37 +0300

    fix: new fail

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
 function App() {
+  debugger
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is app component
+      <Rating />
+      <Accordion />
     </div>
   );
 }
 
+function AppTitle() {
+  return "This is App component"
+}
+
+function Rating() {
+  debugger
+  return (
+    <div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+    </div>
+  )
+}
+
+function Accordion() {
+  debugger
+  return <div>
+    <h3>Меню</h3>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
+
+}
+
+
+
 export default App;
+
+
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,40 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App/>
+  // <div>Hello</div>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// let a = {
+//   name : "Maksim",
+//   age: 10,
+//   address: {
+//     city: "Minsk",
+//     country: "Belarus"
+//   }
+// };
+
+// let a = { 
+//   name: "Maksoniy",
+//   age: 10,
+//   address: {
+//     city: "Minsk",
+//     country: "Belarus"
+//   }
+// };
+
+// let b: Array<string | number>;
+// b = [1,2,3,4,5];
+// b = [];
+// b = [2,4,6,7];
+// b = [2,3, "hello", "yo"];
+// b = [true, false];
+
+
+// console.log(b);
