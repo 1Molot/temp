4c196f48fb63fc2f632e252c2a636c78d7db5ffc
1Molot <sinkevich1990@gmail.com>
2023-04-17 22:17:54 +0300

    fix:useemo

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -10,7 +10,8 @@ const meta = {
     // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
     layout: 'fullscreen',
   },
-} satisfies Meta<typeof Header>;
+}
+//satisfies Meta<typeof Header>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
