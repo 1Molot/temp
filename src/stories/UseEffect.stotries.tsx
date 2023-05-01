6dace44c748a816e21eb74aa189737a6449a0a7f
1Molot <sinkevich1990@gmail.com>
2023-05-01 19:33:50 +0300

    fix:22-useEffect-setTimeout-setInterval

--- src/stories/UseEffect.stotries.tsx
+++ src/stories/UseEffect.stotries.tsx
@@ -33,3 +33,37 @@ export const SimpleExample = () => {
         </>
     )
 }
+
+export const SetTimeoutExample = () => {
+    // const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log('SetTimeoutExample')
+
+    // useEffect(() => {
+    //
+    //     setTimeout(() => {
+    //         console.log('setTimeout')
+    //         document.title = counter.toString();
+    //     },1000);
+    //
+    // },[counter]) //odin paz [],[counter] (при изменении count)
+
+    useEffect(() => {
+
+        setInterval(() => {
+            // console.log('tick: ' + counter)
+            setCounter(state => state + 1);
+        }, 1000);
+
+    }, []) //[] - не регистрирует нет зависимостей( [] )
+
+    return (                                                // JSX
+        <>
+            Hello, counter: {counter}
+            {/*- fake: {fake}*/}
+            {/*<button onClick={() => setFake(fake + 1)}>fake+</button>*/}
+            {/*<button onClick={() => setCounter(counter + 1)}>counter+</button>*/}
+        </>
+    )
+}
